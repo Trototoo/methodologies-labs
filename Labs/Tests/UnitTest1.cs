47fdc9ccd4a3646fd319ba990253fc42f07f3eae
Oleksandr Nazarenko <sa44ko@gmail.com>
2023-03-20 19:38:08 +0200

    Created two methods for easier list creation in future tests.

--- Labs/Tests/UnitTest1.cs
+++ Labs/Tests/UnitTest1.cs
@@ -1,8 +1,33 @@
+using Lab2;
+
 namespace Tests;
 
 [TestClass]
 public class UnitTest1
 {
+
+    private CustomList<char> CreateTestList()
+    {
+        CustomList<char> list = new CustomList<char>();
+        list.Append('a');
+        list.Append('b');
+        list.Append('c');
+        list.Append('d');
+        list.Append('e');
+        return list;
+    }
+    
+    private CustomList<char> CreateTestListWithSameChars()
+    {
+        CustomList<char> list = new CustomList<char>();
+        list.Append('a');
+        list.Append('a');
+        list.Append('a');
+        list.Append('b');
+        list.Append('b');
+        return list;
+    }
+    
     [TestMethod]
     public void TestMethod1()
     {
